8794df3efc05510a8bc9328eda2cf2e1535519dc
John Jeneiro <johnjeneiro@gmail.com>
2021-09-13 14:48:57 +0100

    Update index.jsx

--- task_1/index.jsx
+++ task_1/index.jsx
@@ -1,16 +1,19 @@
-import { useState, Fragment } from 'react';
+import { useState, Fragment, useEffect } from 'react';
 
-const MainComponent = () => {
-    const [ _, update ] = useState(); // change state for force component rerendering and checking the result
-
+const MainComponent =()=> {
+    const [ _, update ] = useState(""); // change state for force component rerendering and checking the result
+useEffect(()=>{update("Slim Eminem")},[])// Initial State
     return (
         <Fragment>
-            <button onCLick={update}>update MainComponent</button>
-            <ChildComponent name={'Slim shady'} />
+            <button onClick={()=>{update("Slim Shaddy")}}>update MainComponent</button>
+            <ChildComponent name={_} />
         </Fragment>
     );
 };
 
 const ChildComponent = ({ name }) => (
+  
     <div>my name is {name}</div>
 );
+
+export default MainComponent;
